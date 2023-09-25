f06e7adade9ed643c2c79ae1fc74cddd9e5f93c3
Atallah <Atallah.192000@gmail.com>
2023-09-26 02:25:16 +0300

    Authentication by Passport.js Provided

---
+++ src/auth/passport.ts
@@ -0,0 +1,28 @@
+import passport from 'passport';
+import passportJWT, { StrategyOptions } from 'passport-jwt';
+import { ExtractJwt } from 'passport-jwt';
+import User from '../models/customerModel';
+const config = require('../../config/config.json');
+
+const ExtractJWT = ExtractJwt;
+const StrategyJWT = passportJWT.Strategy;
+
+const jwtOptions: StrategyOptions = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+    secretOrKey: config.development.jwt_secret
+};
+
+passport.use(new StrategyJWT(jwtOptions, async (jwtPayload, done) => {
+    try {
+        const user = await User.findOne({ where: { id: jwtPayload.id } });
+        if (user) {
+            return done(null, user);
+        } else {
+            return done(null, false);
+        }
+    } catch (error) {
+        return done(error, false);
+    }
+}));
+
+export default passport;
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import express, { Request, Response } from 'express';
+// Auth
+import morgan from 'morgan';
+import helmet from 'helmet';
+import cors from 'cors';
+import dotenv from 'dotenv'; // Use 'dotenv' instead of 'require'
+import passport from 'passport';
+import './auth/passport'; // Import and execute your passport configuration
 
-const app = express()
+dotenv.config(); // Call 'config()' method from 'dotenv'
+
+const app = express();
 
 // Middlewares
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(morgan('dev'));
+app.use(helmet()); // Call 'helmet' as a function to enable the middleware
+app.use(cors());
 
 // Routes
 import books from './routes/bookRoutes';
@@ -13,11 +25,10 @@ import customers from './routes/customerRoutes';
 import register from './api/register';
 import login from './api/login';
 
-
 // Middleware routes
-app.use(register)
-app.use(login)
-app.use('/api/books', books);
+app.use(register);
+app.use(login);
+app.use('/api/books', passport.authenticate('jwt', { session: false }), books);
 app.use('/api/customers', customers);
 
 app.use((err: any, req: Request, res: Response, next: any) => {
@@ -25,7 +36,7 @@ app.use((err: any, req: Request, res: Response, next: any) => {
     res.status(500).send('Something went wrong!');
 });
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}...`);
-})
+});
